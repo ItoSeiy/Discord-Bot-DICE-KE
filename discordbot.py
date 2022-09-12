dc5822b015f1f66faabde49b335973f8706b6b8d
伊東聖矢 <itoseiya1219@gmail.com>
2022-09-12 13:45:53 +0900

    ダイス修正

--- discordbot.py
+++ discordbot.py
@@ -10,40 +10,31 @@ client = discord.Client(intents=Intents)
 
 @client.event
 async def on_message(message : discord.message.Message):
-    if(message.author.bot):
-        return
-    if(message.content[0]  != "/"):
+    content = message.content
+    content = content.replace(" ", "")
+    if(message.author.bot or content[0]  != "/"):
         return
 
-    if("/dice" in message.content):
-        try:
-            text = message.content[6:]
-            texts = text.split("d")
-            roll_time = int(texts[0])
-            face_num = int(texts[1])
-            dice_nums = []
-            total_num:int = 0
+    try:
+        texts = content.split("/")[1].split("d")
+        roll_time = int(texts[0])
+        face_num = int(texts[1])
+        dice_nums = []
+        total_num:int = 0
 
-            if roll_time == 1:
+        if roll_time == 1:
+            num = random.randint(0,  face_num)
+            dice_nums.append(num)
+            total_num = num
+        elif roll_time > 1:
+            for i in range(roll_time):
                 num = random.randint(0,  face_num)
                 dice_nums.append(num)
-                total_num = num
-            elif roll_time > 1:
-                for i in range(roll_time):
-                    num = random.randint(0,  face_num)
-                    dice_nums.append(num)
-                    total_num += num
-            else:
-                raise Exception
-            await message.reply(f"{total_num} {dice_nums}")
-        except Exception as e:
-            await message.reply("ダイスの値が不正です")
-            print("=== エラー内容 ===")
-            print("type:" + str(type(e)))
-            print('args:' + str(e.args))
-            print('message:' + e.message)
-            print('e自身:' + str(e))
-    else:
-            await message.reply("コマンドが不正です")
-
+                total_num += num
+        else:
+            raise Exception
+        await message.reply(f"{total_num} {dice_nums}")
+    except Exception as e:
+        await message.reply("ダイスの値が不正です")
+        await message.reply(f"===エラー内容===\n type:{type(e)}\n args:{e.args}\n message:{e.message}\n error:{e}")
 client.run(discord_token.TOKEN)
